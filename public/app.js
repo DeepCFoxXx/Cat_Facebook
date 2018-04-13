1091dab5f1d521f5742ee7924d61b3bbb73040f6
DeepCFoxXx <learncode23@gmail.com>
2018-04-13 15:54:15 +0100

    cat data added

---
+++ public/app.js
@@ -0,0 +1,22 @@
+var CAT_DATA = [
+  {
+    name:"Mario",
+    favFood: "Ham",
+    img: "http://www.catster.com/wp-content/uploads/2017/08/Pixiebob-cat.jpg"
+  },
+  {
+    name:"Spot",
+    favFood: "Replicated tuna",
+    img: "http://www.catster.com/wp-content/uploads/2017/11/Angry-cat-growling-or-hissing-ears-back.jpg"
+  },
+  {
+    name:"Sasha",
+    favFood: "Steak",
+    img: "http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg"
+  },
+  {
+    name:"Rosalina",
+    favFood: "Raw vegan tiramisu slice",
+    img: "http://www.catster.com/wp-content/uploads/2017/08/A-brown-cat-licking-its-lips.jpg"
+  }
+];
